fb4335c23cfa681f1454d369dddb9374b7ef917d
Mateus Pandino <matpandino@gmail.com>
2023-05-24 03:30:45 -0300

    add search input

--- web/src/components/Search/Search.tsx
+++ web/src/components/Search/Search.tsx
@@ -1,17 +1,22 @@
 import Image from "next/image";
 import React from "react";
+import SearchInput from "./components/SearchInput";
 
 export default function Search() {
   return (
     <div className="flex h-20 w-full max-w-[92%] flex-row items-center justify-between">
-      <Image
-        src="/hostshare-green.png"
-        alt="hostshare green logo"
-        width="160"
-        height="70"
-      />
-      <div>Search</div>
-      <div>ProfileInfo</div>
+      <div className="flex-1">
+        <Image
+          src="/hostshare-green.png"
+          alt="hostshare green logo"
+          width="183"
+          height="40"
+        />
+      </div>
+      <SearchInput />
+      <div className="flex flex-1 justify-end">
+        <span className="block">Profile</span>
+      </div>
     </div>
   );
 }
---
+++ web/src/components/Search/components/SearchInput.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+
+export default function SearchInput() {
+  return (
+    <div className="placeholderL:text-gray-500 flex items-center justify-center gap-2 rounded-3xl border border-gray-200 bg-white py-2 pe-2 ps-4 text-sm shadow-md">
+      <input className="focus:outline-none" placeholder="Search..." />
+      <div className="flex aspect-square h-8 items-center justify-center rounded-full bg-primary text-white">
+        <FaSearch />
+      </div>
+    </div>
+  );
+}
