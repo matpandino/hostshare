c531641cfa1323e683644dcc4742270136b91162
Mateus Pandino <matpandino@gmail.com>
2023-05-26 02:05:21 -0300

    chore: limit categories call

--- web/src/components/CategoriesFilter/CategoriesFilter.tsx
+++ web/src/components/CategoriesFilter/CategoriesFilter.tsx
@@ -2,7 +2,10 @@ import { type Category } from "@/types";
 import CategoryItem from "./component/CategoryItem";
 
 export default async function CategoriesFilter() {
-  const response = await fetch("http://localhost:3003/categories");
+  const response = await fetch(
+    "http://localhost:3003/categories?_page=1&_limit=12"
+  );
+
   const categories = (await response.json()) as Category[];
 
   return (
