5473ec2be662569dbd917cc675a4653b8e43d5b3
Mateus Pandino <matpandino@gmail.com>
2023-05-29 00:09:42 -0300

    add QuantitySelector component

---
+++ web/src/components/QuantitySelector.tsx
@@ -0,0 +1,51 @@
+import { type FC, type ReactNode } from "react";
+
+interface QuantitySelectorProps {
+  min: number;
+  max: number;
+  value: number;
+  afterValue?: ReactNode;
+  onChange: (value: number) => void;
+}
+
+const QuantitySelector: FC<QuantitySelectorProps> = ({
+  min,
+  max,
+  value,
+  afterValue,
+  onChange,
+}) => {
+  const handleDecrease = () => {
+    if (value > min) {
+      onChange(value - 1);
+    }
+  };
+
+  const handleIncrease = () => {
+    if (value < max) {
+      onChange(value + 1);
+    }
+  };
+
+  return (
+    <div className="flex items-center overflow-hidden rounded-md border">
+      <button
+        className="w-5 bg-gray-100 text-lg hover:bg-gray-200 focus:outline-none"
+        onClick={handleDecrease}
+      >
+        -
+      </button>
+      <span className="px-1 text-sm font-semibold">
+        {value} {afterValue}
+      </span>
+      <button
+        className="w-5 bg-gray-100 text-lg hover:bg-gray-200 focus:outline-none"
+        onClick={handleIncrease}
+      >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default QuantitySelector;
