25f3ae8037e957dde79fc05c4400bd101b1df0f6
Mateus Pandino <matpandino@gmail.com>
2023-05-29 06:42:08 -0300

    fix json-server host setup

--- api/server.js
+++ api/server.js
@@ -1,21 +1,18 @@
-// JSON Server module
+// See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
-
-// Make sure to use the default middleware
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
-  // Add custom route here if needed
   jsonServer.rewriter({
-    "/*": "/$1",
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
   })
 );
 server.use(router);
-// Listen to port
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
